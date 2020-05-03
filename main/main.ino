d4f1196aea82ccd5e38ac424d6e67836b14b8906
Paolo Martinelli - PM Control <info@pm-control.tech>
2020-05-03 08:35:24 +0200

    varie
    inserito commenti
    modificato :
    lc => led_timer
    t => potenziometro_tempo

--- main/main.ino
+++ main/main.ino
@@ -1,16 +1,15 @@
 // programma irrigazione S. Marco
 
 
-
 int reset = 13; // pin reset tempo
 int tastiera1 = A1;  // tastiera zone (
 
-const int A = 9;
-const int B = 10;
-const int C = 11;
-const int D = 12;
+const int A = 9;  // uscita binaria per elettrovalvole
+const int B = 10; // uscita binaria per elettrovalvole
+const int C = 11; // uscita binaria per elettrovalvole
+const int D = 12; // uscita binaria per elettrovalvole
 
-int t = A0; // pin potenziometro per regolare tempo
+int potenziometro_tempo = A0; // pin potenziometro per regolare tempo
 
 int l1 = 5; // pin led zona1
 int l2 = 6; // pin led zona2
@@ -20,9 +19,9 @@ int l3 = 7; // pin led zona3
 //int 16 = 8 // pin led zona6
 //int l7 = 9 // pin led zona7
 
-int lc = 8; // pin led controllo tempo > 0
+int led_timer = 8; // pin led controllo tempo > 0
 
-unsigned long tempo; // tempo analogRead(t)enziometro (di base = 0)
+unsigned long tempo; // tempo analogRead(potenziometro_tempo)enziometro (di base = 0)
 unsigned long tempo_att = 0; // tempo durata azione (di base = 0)
 
 void setup() {
@@ -55,7 +54,7 @@ void setup() {
   //pinMode(l6, OUTPUT);
   //pinMode(l7, OUTPUT);
   
-  pinMode(lc, OUTPUT); // accesa = tempo scelto; spenta = tempo non scelto
+  pinMode(led_timer, OUTPUT); // accesa = tempo scelto; spenta = tempo non scelto
 
 }
 
@@ -63,7 +62,7 @@ void loop() {
   // put your main code here, to run repeatedly
 
   Serial.print("Val pot : ");
-  Serial.println(analogRead(t));
+  Serial.println(analogRead(potenziometro_tempo));
   Serial.print("tempo : ");
   Serial.println(tempo);
   Serial.print("tastiera1 : ");
@@ -72,33 +71,33 @@ void loop() {
 
   delay(2000);
 
-  if (analogRead(t) < 1){
+  if (analogRead(potenziometro_tempo) < 1){
     tempo = 0;
     Serial.println("0 minuti");
   }
 
-  if ( analogRead(t) > 1 && analogRead(t) <325 ){ //posizione 5 min analogRead(t)enziometro
+  if ( analogRead(potenziometro_tempo) > 1 && analogRead(potenziometro_tempo) <325 ){ //posizione 5 min analogRead(potenziometro_tempo)enziometro
 
     tempo = 30000; // 30 secondi
     Serial.println("30 secondi"); 
     
   }
 
-  if ( analogRead(t) > 325 && analogRead(t) < 650 ){  //posizione 10 min analogRead(t)enziometro
+  if ( analogRead(potenziometro_tempo) > 325 && analogRead(potenziometro_tempo) < 650 ){  //posizione 10 min analogRead(potenziometro_tempo)enziometro
 
     tempo = 60000; // 1 minuto
     Serial.println("1 minuto");
     
   }
 
-  if ( analogRead(t) > 650 && analogRead(t) < 975 ){  //posizione 15 min analogRead(t)enziometro
+  if ( analogRead(potenziometro_tempo) > 650 && analogRead(potenziometro_tempo) < 975 ){  //posizione 15 min analogRead(potenziometro_tempo)enziometro
 
     tempo = 90000; // 1 minuto 30 secondi
     Serial.println("1,5 minuti");
     
   }
 
-  if ( analogRead(t) > 975 ){  //posizione 20 min analogRead(t)enziometro
+  if ( analogRead(potenziometro_tempo) > 975 ){  //posizione 20 min analogRead(potenziometro_tempo)enziometro
 
     tempo = 120000; // 2 minuti
     Serial.println("2 minuti");
@@ -106,9 +105,9 @@ void loop() {
   }
 
   if (tempo < 30000){
-    digitalWrite(lc, LOW);
+    digitalWrite(led_timer, LOW);
   }else{
-    digitalWrite(lc, HIGH);
+    digitalWrite(led_timer, HIGH);
     if (analogRead(tastiera1)>0 && analogRead(tastiera1)<10){
       
       digitalWrite(l1, HIGH);
@@ -194,7 +193,6 @@ void loop() {
       digitalWrite(C, HIGH);
       digitalWrite(D, HIGH);
       
-    }
-    
+    }    
   }
 }
